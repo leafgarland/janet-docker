69669c29c2c03b1ecee0ac0a63899e9f97dabf46
Leaf Garland <leafgarland@users.noreply.github.com>
2020-06-25 07:34:20 +1200

    Fix bug so that we push and commit

--- docker-janet.sh
+++ docker-janet.sh
@@ -38,18 +38,17 @@ CURRENT_TAG=$(curl -L -s -H 'Accept: application/json' https://api.github.com/re
 
 if [ "$LAST_TAG" == "$CURRENT_TAG" ]; then
     echo "No new tags since $CURRENT_TAG"
-    exit 0
-fi
-
-CURRENT_TAG_COMMIT=$(curl -L -s -H 'Accept: application/json' https://api.github.com/repos/janet-lang/janet/tags | jq -j .[0].commit.sha)
-
-if [ "$CURRENT_TAG_COMIT" == "$CURRENT_COMMIT" ]; then
-    echo "Tagging image for latest tag $CURRENT_TAG, last tag was $LAST_TAG"
-    docker tag $DOCKER_REPO/janet:latest $DOCKER_REPO/janet:$CURRENT_TAG
-    docker tag $DOCKER_REPO/janet-sdk:latest $DOCKER_REPO/janet-sdk:$CURRENT_TAG
 else
-    echo "Building new image for $CURRENT_TAG at commit $CURRENT_TAG_COMMIT"
-    docker-build $CURRENT_TAG $CURRENT_TAG_COMMIT
+    CURRENT_TAG_COMMIT=$(curl -L -s -H 'Accept: application/json' https://api.github.com/repos/janet-lang/janet/tags | jq -j .[0].commit.sha)
+
+    if [ "$CURRENT_TAG_COMIT" == "$CURRENT_COMMIT" ]; then
+        echo "Tagging image for latest tag $CURRENT_TAG, last tag was $LAST_TAG"
+        docker tag $DOCKER_REPO/janet:latest $DOCKER_REPO/janet:$CURRENT_TAG
+        docker tag $DOCKER_REPO/janet-sdk:latest $DOCKER_REPO/janet-sdk:$CURRENT_TAG
+    else
+        echo "Building new image for $CURRENT_TAG at commit $CURRENT_TAG_COMMIT"
+        docker-build $CURRENT_TAG $CURRENT_TAG_COMMIT
+    fi
 fi
 
 echo $DOCKER_PASSWORD | docker login -u leafgarland --password-stdin
@@ -58,7 +57,6 @@ docker push $DOCKER_REPO/janet-sdk
 
 echo $CURRENT_COMMIT > last_commit.txt
 echo $CURRENT_TAG > last_tag.txt
-
 
 if ! git diff --no-ext-diff --quiet --exit-code; then
     git add -A && git commit -m 'Update last_commit' --allow-empty && git push -u origin master
